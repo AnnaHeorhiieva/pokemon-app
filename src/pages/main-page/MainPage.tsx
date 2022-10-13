cc7ca90cbbbe942ec0876adc3fc564c6af4ead03
Anna Heorhiieva <anna.heorhiieva@gmail.com>
2022-10-13 13:44:25 +0300

    first commit

---
+++ src/pages/main-page/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./MainPage.module.scss";
+
+function MainPage() {
+  return <div className={styles.mainPage}>Pokemon app</div>;
+}
+
+export default MainPage;

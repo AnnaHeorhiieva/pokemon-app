eadd6ce556dd059af16089ae6151b08a62f8bac1
Anna Heorhiieva <anna.heorhiieva@gmail.com>
2022-10-13 17:16:54 +0300

    feat(pages): page PokemonDetailsPage has been created

---
+++ src/pages/pokemon-details-page/PokemonDetailsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PokemonDetailsPage.module.scss";
+
+function PokemonDetailsPage() {
+  return <div className={styles.pokemonDetailsPage}>Pokemon details</div>;
+}
+
+export default PokemonDetailsPage;

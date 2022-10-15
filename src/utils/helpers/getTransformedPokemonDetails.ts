db6d88c1449c856ce9b0b3173d765cb52426470a
Anna Heorhiieva <anna.heorhiieva@gmail.com>
2022-10-15 13:57:08 +0300

    feat(helpers): created function for pokeapi incoming pokemon object transformation to the necessary format

---
+++ src/utils/helpers/getTransformedPokemonDetails.ts
@@ -0,0 +1,75 @@
+import {
+  Pokemon,
+  PokemonAbility,
+  PokemonMove,
+  PokemonStat,
+} from "store/pokemon/types";
+import { SvgIcon } from "@mui/material";
+
+import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import HeightIcon from "@mui/icons-material/Height";
+import ScaleIcon from "@mui/icons-material/Scale";
+
+import { addUnitsToNumericValues } from "./addUnitsToNumericValues";
+import { formArrayOfNames } from "./formArrayOfNames";
+
+export type MUIIcon = typeof SvgIcon;
+
+export interface MainHeaderPokemonDetails {
+  name: string;
+  photo: string;
+}
+
+export interface MainBodyPokemonDetails {
+  experience: [number, MUIIcon];
+  height: [string, MUIIcon];
+  weight: [string, MUIIcon];
+}
+
+export interface MainPokemonDetails {
+  mainHeaderDetails: MainHeaderPokemonDetails;
+  mainBodyDetails: MainBodyPokemonDetails;
+}
+
+export interface SecondaryPokemonDetails {
+  [key: string]: string[];
+  abilities: string[];
+  moves: string[];
+  stats: string[];
+}
+
+export interface PokemonDetails {
+  mainDetails: MainPokemonDetails;
+  secondaryDetails: SecondaryPokemonDetails;
+}
+
+export function getTransformedPokemonDetails(initialPokemonObject: Pokemon) {
+  const { name, base_experience, height, weight } = initialPokemonObject;
+  const { abilities, moves, stats, sprites } = initialPokemonObject;
+
+  const nameUppercased = name?.toUpperCase();
+  const photoOptions = sprites?.front_default ?? sprites?.front_shiny;
+  const heightWithUnits = addUnitsToNumericValues(height, "decimetres");
+  const weightWithUnits = addUnitsToNumericValues(weight, "hectograms");
+
+  const mainHeaderDetails = { name: nameUppercased, photo: photoOptions };
+
+  const mainBodyDetails = {
+    experience: [base_experience, CatchingPokemonIcon],
+    height: [heightWithUnits, HeightIcon],
+    weight: [weightWithUnits, ScaleIcon],
+  };
+
+  const mainDetails = { mainHeaderDetails, mainBodyDetails };
+
+  const secondaryDetails = {
+    abilities: formArrayOfNames<PokemonAbility>(abilities, "ability"),
+    moves: formArrayOfNames<PokemonMove>(moves, "move"),
+    stats: formArrayOfNames<PokemonStat>(stats, "stat"),
+  };
+
+  return {
+    mainDetails,
+    secondaryDetails,
+  } as PokemonDetails;
+}
